f760ab2e8871fd19414b6d2f9d8ae4dde69f3b77
Jo <martis.joey@gmail.com>
2020-01-30 02:06:03 +0100

    Eerste poging App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap} from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Pokemon } from '../shared/model/pokemon';
+import { PokemonService } from 'src/shared/services/pokemon.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokemon';
+  public pokemon$: Observable<Pokemon[]>;
+  currentpokemon$: Observable<Pokemon>;
+constructor(private pokemonService: PokemonService){}
+ngOnInit(){};
 }
+
+// export class AppComponent implements OnInit {
+//   countries$: Observable<Country[]>;
+//   currentcountry$: Observable<Country>   
+//   constructor(private countryService  : CountryService) {}
+//   ngOnInit() {this.countries$ = this.countryService.getCountries();}
+//   getCountry(id  : number) {console.log('boo');}
+//   // Land toevoegen --> doorgeven aan de service
+//   addCountry (countryName  : string, continentName  : string) {
+//     // id === null, omdat deze auto wordt ingevuld door de json server
+//     const newCountry = new Country(null, countryName, continentName);
+//     this.countryService.addCountry(newCountry)
+//     .subscribe ((addedCountry  : Country) => {
+//       // countries opnieuw ophalen in de subscription
+//       this.countries$ = this.countryService.getCountries();
+//     }); } 
+//     // Land verwijderen --> doorgeven aan de service
+//     deleteCountry (country  : Country) {   //TODO 
+//     } } 
